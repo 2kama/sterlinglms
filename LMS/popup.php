d11d149b10d3ce89d765de101b624d25c55a6add
Mishael Kama <mishealkama@gmail.com>
2018-07-18 14:10:16 +0100

    popup

---
+++ LMS/popup.php
@@ -0,0 +1,257 @@
+<html lang="en">
+  <head>
+    <title>Hello, world!</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  </head>
+  <body>
+    
+
+
+
+
+
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
+
+      <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLongTitle" style="color:#6256A2; ">Quiz Attempts</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  
+                    <div class="row drape">
+                      <div class="col-md-3">
+                          <div class="nav flex-column nav-pills init" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                          <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">
+                              <div class="dry col-md-12">Quiz 1</div>
+                              <div class="wet col-md-12"># of attempts: 3 <br>Highest score: 75%</div>
+                          </a>
+                          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">
+                               <div class="dry col-md-12">Quiz 2</div>
+                              <div class="wet col-md-12"># of attempts: 3 <br>Highest score: 75%</div>
+                          </a>
+                          <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">
+                                <div class="dry col-md-12">Quiz 3</div>
+                              <div class="wet col-md-12"># of attempts: 3 <br>Highest score: 75%</div>
+                          </a>
+                          <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">
+                                 <div class="dry col-md-12">Quiz 4</div>
+                              <div class="wet col-md-12"># of attempts: 3 <br>Highest score: 75%</div>
+                          </a>
+                        </div>
+                      </div>
+
+
+                      <div class="col-md-9">
+                        <div class="tab-content" id="v-pills-tabContent">
+                          
+
+                          <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                            
+                                
+                                <ul class="nav nav-pills mb-3 lant" id="pills-tab" role="tablist">
+                                  <li class="nav-item">
+                                    <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Attempt 1</a>
+                                  </li>
+                                  <li class="nav-item">
+                                    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Attempt 2</a>
+                                  </li>
+                                  <li class="nav-item">
+                                    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Attempt 3</a>
+                                  </li>
+                                </ul>
+
+                                <div class="tab-content" id="pills-tabContent">
+                                 
+
+                                  <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                                      <div class="row">
+                                          <div class="col-md-9"><h6>Summary</h6></div>
+                                          <div class="col-md-9">
+                                              <table class="table table-striped">
+                                                  <tbody>
+                                                      <tr>
+                                                        <td>Scored:</td>
+                                                        <td>40</td>
+                                                      </tr>
+                                                      <tr>
+                                                        <td>Maximum Score:</td>
+                                                        <td>40</td>
+                                                      </tr>
+                                                      <tr>
+                                                        <td>Correct Questions:</td>
+                                                        <td>5</td>
+                                                      </tr>
+                                                      <tr>
+                                                        <td>Total Questions:</td>
+                                                        <td>5</td>
+                                                      </tr>
+                                                      <tr>
+                                                        <td>Accuracy:</td>
+                                                        <td>100%</td>
+                                                      </tr>
+                                                  </tbody>
+                                              </table>
+                                          </div>
+
+                                          <div class="col-md-9"><h6>Responses</h6></div>
+
+                                          <div class="col-md-9">
+
+                                             <div class="col-md-12 qqq">
+                                                 <sup class="col-md-12">Question 1 of 4</sup>
+                                                 <span>What is the company's slogan</span>
+                                                 <div class="col-md-12">
+                                                   <span class="blue">Your answer: </span><span>We win always <i class="material-icons" style="color: red">close</i></span>
+                                                 </div>
+                                             </div>
+
+                                             <div class="col-md-12 qqq">
+                                                 <sup class="col-md-12">Question 1 of 4</sup>
+                                                 <span>What is the company's slogan</span>
+                                                 <div class="col-md-12">
+                                                   <span class="blue">Your answer: </span><span>We win always <i class="material-icons" style="color: green">checkmark</i></span>
+                                                 </div>
+                                             </div>
+
+
+                                          </div>
+                                      </div>
+                                  </div>
+
+
+
+                                  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
+                                  <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+                                </div>
+
+
+                          </div>
+
+
+
+
+                          <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+                          <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+                          <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+                        </div>
+                      </div>
+
+
+                    </div>
+
+                        
+
+
+                        
+
+
+
+                </div>
+
+
+          </div>
+        </div>
+      </div>
+          
+
+
+<style>
+   .qqq {
+     margin-bottom: 20px;
+     padding: 0px;
+   }
+   .qqq sup {
+     color: #ccc;
+     font-size: 12px;
+     width: 100%;
+     float: left;
+     padding: 0px;
+     margin-top: 5px;
+   }
+   .qqq span {
+      font-weight: 500;
+      color: #555;
+      font-size: 14px;
+   }
+   .qqq .col-md-12 {
+      padding: 0px;
+   }
+   .qqq .blue {
+     color: blue;
+   }
+   .init {
+      border-right: 1px solid #ccc;
+   }
+   .drape .col-md-3 a {
+     border: none;
+     border-radius: 0px;
+     -webkit-border-radius: 0px;
+   }
+    .drape .col-md-3 a.active {
+       background: #fff !important;
+       border-right: 2px solid #354C98;
+       border-radius: 0px;
+     -webkit-border-radius: 0px;
+    }
+    .drape .col-md-3 a .dry {
+        color: #aaa;
+        font-size: 15px;
+        font-weight: bold;
+    }
+    .drape .col-md-3 a .wet {
+        color: rgba(26, 26, 26, 0.5);
+        font-size: 12px;
+    }
+    .drape .col-md-3 a.active .dry {
+        color: #354C98;
+        font-size: 15px;
+        font-weight: bold;
+    }
+    .drape .col-md-3 a.active .wet {
+       color: rgba(98, 86, 162, 0.5);
+       font-size: 12px;
+    }
+
+
+    .lant a {
+       background: #D8D8D8;
+       color: #fff;
+       border-radius: 20px !important;
+       -webkit-border-radius: 20px !important;
+       font-size: 12px;
+       padding: 10px;
+       margin-left: 15px;
+       font-weight: 500;
+    }
+    .lant a.active {
+       background: #354C98 !important;
+
+
+    }
+
+
+
+</style>
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
